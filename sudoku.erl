b265a31dc3464396827f5a29aa9955f6c9f9e98c
Andreas Pauley <apauley+github@gmail.com>
2011-02-13 15:18:07 +0200

    define row_squares()

--- sudoku.erl
+++ sudoku.erl
@@ -5,6 +5,7 @@ test() ->
     ["A1","A2","B1","B2"] = cross("AB", "12"),
     81 = length(squares()),
     [["A1","B1","C1","D1","E1","F1","G1","H1","I1"]|_] = col_squares(),
+    [["A1","A2","A3","A4","A5","A6","A7","A8","A9"]|_] = row_squares(),
     ok.
 
 cross(SeqA, SeqB) ->
@@ -23,3 +24,5 @@ squares() ->
 
 col_squares() ->
     [cross(rows(), [C]) || C <- cols()].
+row_squares() ->
+    [cross([R], cols()) || R <- rows()].
