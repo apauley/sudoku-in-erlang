8af7872843e8652b9e71a782731b1db02609e711
Andreas Pauley <apauley+github@gmail.com>
2011-02-13 15:13:36 +0200

    Compact lists

--- sudoku.erl
+++ sudoku.erl
@@ -2,9 +2,9 @@
 -compile(export_all).
 
 test() ->
-    ["A1", "A2", "B1", "B2"] = cross("AB", "12"),
+    ["A1","A2","B1","B2"] = cross("AB", "12"),
     81 = length(squares()),
-    [["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1"]|_] = col_squares(),
+    [["A1","B1","C1","D1","E1","F1","G1","H1","I1"]|_] = col_squares(),
     ok.
 
 cross(SeqA, SeqB) ->
