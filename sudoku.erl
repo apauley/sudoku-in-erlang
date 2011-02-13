3edcb2dea23bfba1c876c65d9a5f3c19ee62a21a
Andreas Pauley <apauley+github@gmail.com>
2011-02-13 15:00:06 +0200

    define squares()

--- sudoku.erl
+++ sudoku.erl
@@ -3,8 +3,19 @@
 
 test() ->
     ["A1", "A2", "B1", "B2"] = cross("AB", "12"),
+    81 = length(squares()),
     ok.
 
 cross(SeqA, SeqB) ->
     %% Cross product of elements in SeqA and elements in SeqB.
     [[X,Y] || X <- SeqA, Y <- SeqB].
+
+digits() ->
+    "123456789".
+rows() ->
+    "ABCDEFGHI".
+cols() ->
+    digits().
+
+squares() ->
+    cross(rows(), cols()).
