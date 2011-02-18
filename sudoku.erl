8fd7980e6694c5fbb2fe7ccf114cc649fad0cef0
Andreas Pauley <apauley+github@gmail.com>
2011-02-18 09:12:09 +0200

    Move low-level functions to the bottom

--- sudoku.erl
+++ sudoku.erl
@@ -2,6 +2,67 @@
 -import(lists, [member/2]).
 -compile(export_all).
 
+solve(GridString) ->
+    display(parse_grid(GridString)).
+
+assign(ValuesDict, Square, Digit) ->
+    %% Assign by eliminating all values except the assigned value.
+    OtherValues = lists:delete(Digit, dict:fetch(Square, ValuesDict)),
+    eliminate(ValuesDict, [Square], OtherValues).
+
+eliminate(ValuesDict, [], _) ->
+    ValuesDict;
+
+eliminate(ValuesDict, Squares, Digits) ->
+    %% Eliminate the specified Digits from all specified Squares.
+    [Square|T] = Squares,
+    OldValues = dict:fetch(Square, ValuesDict),
+    NewValues = lists:filter(fun(E) -> not member(E, Digits) end, OldValues),
+    NewDict1 = dict:store(Square, NewValues, ValuesDict),
+    NewDict2 = peer_eliminate(NewDict1, Square, NewValues, OldValues),
+    eliminate(NewDict2, T, Digits).
+
+peer_eliminate(ValuesDict, _, Vals, Vals) ->
+    %% NewValues and OldValues are the same, already eliminated.
+    ValuesDict;
+
+peer_eliminate(ValuesDict, Square, [AssignedValue], _) ->
+    %% If there is only one value left, we can also
+    %% eliminate that value from the peers of Square
+    Peers = peers(Square),
+    eliminate(ValuesDict, Peers, [AssignedValue]);
+
+peer_eliminate(ValuesDict, _, _, _) ->
+    %% Multiple values, cannot eliminate from peers.
+    ValuesDict.
+
+display(ValuesDict) ->
+    Fun = fun({_, [V]}) -> [V];
+             ({_, _}) -> "."
+          end,
+    lists:flatmap(Fun, lists:sort(dict:to_list(ValuesDict))).
+
+parse_grid(GridString) ->
+    parsed_dict(empty_dict(), squares(), GridString).
+
+parsed_dict(ValuesDict, [], []) ->
+    ValuesDict;
+parsed_dict(ValuesDict, [Square|Squares], [Value|GridString]) ->
+    IsDigit = member(Value, digits()),
+    NewDict = assign_if_possible(ValuesDict, Square, Value, IsDigit),
+    parsed_dict(NewDict, Squares, GridString).
+
+assign_if_possible(ValuesDict, Square, Value, true) ->
+    %% Value is a Digit, possible to assign
+    assign(ValuesDict, Square, Value);
+assign_if_possible(ValuesDict, _, _, false) ->
+    %% Not possible to assign
+    ValuesDict.
+
+empty_dict() ->
+    Digits = digits(),
+    dict:from_list([{Square, Digits} || Square <- squares()]).
+
 cross(SeqA, SeqB) ->
     %% Cross product of elements in SeqA and elements in SeqB.
     [[X,Y] || X <- SeqA, Y <- SeqB].
@@ -48,64 +109,3 @@ shallow_flatten([]) -> [];
 shallow_flatten(List) ->
     [H|T] = List,
     H ++ shallow_flatten(T).
-
-parse_grid(GridString) ->
-    parsed_dict(empty_dict(), squares(), GridString).
-
-parsed_dict(ValuesDict, [], []) ->
-    ValuesDict;
-parsed_dict(ValuesDict, [Square|Squares], [Value|GridString]) ->
-    IsDigit = member(Value, digits()),
-    NewDict = assign_if_possible(ValuesDict, Square, Value, IsDigit),
-    parsed_dict(NewDict, Squares, GridString).
-
-assign_if_possible(ValuesDict, Square, Value, true) ->
-    %% Value is a Digit, possible to assign
-    assign(ValuesDict, Square, Value);
-assign_if_possible(ValuesDict, _, _, false) ->
-    %% Not possible to assign
-    ValuesDict.
-
-empty_dict() ->
-    Digits = digits(),
-    dict:from_list([{Square, Digits} || Square <- squares()]).
-
-assign(ValuesDict, Square, Digit) ->
-    %% Assign by eliminating all values except the assigned value.
-    OtherValues = lists:delete(Digit, dict:fetch(Square, ValuesDict)),
-    eliminate(ValuesDict, [Square], OtherValues).
-
-eliminate(ValuesDict, [], _) ->
-    ValuesDict;
-
-eliminate(ValuesDict, Squares, Digits) ->
-    %% Eliminate the specified Digits from all specified Squares.
-    [Square|T] = Squares,
-    OldValues = dict:fetch(Square, ValuesDict),
-    NewValues = lists:filter(fun(E) -> not member(E, Digits) end, OldValues),
-    NewDict1 = dict:store(Square, NewValues, ValuesDict),
-    NewDict2 = peer_eliminate(NewDict1, Square, NewValues, OldValues),
-    eliminate(NewDict2, T, Digits).
-
-peer_eliminate(ValuesDict, _, Vals, Vals) ->
-    %% NewValues and OldValues are the same, already eliminated.
-    ValuesDict;
-
-peer_eliminate(ValuesDict, Square, [AssignedValue], _) ->
-    %% If there is only one value left, we can also
-    %% eliminate that value from the peers of Square
-    Peers = peers(Square),
-    eliminate(ValuesDict, Peers, [AssignedValue]);
-
-peer_eliminate(ValuesDict, _, _, _) ->
-    %% Multiple values, cannot eliminate from peers.
-    ValuesDict.
-
-display(ValuesDict) ->
-    Fun = fun({_, [V]}) -> [V];
-             ({_, _}) -> "."
-          end,
-    lists:flatmap(Fun, lists:sort(dict:to_list(ValuesDict))).
-
-solve(GridString) ->
-    display(parse_grid(GridString)).
